4accf925532cb1d75187efbdf42731ddcb225914
MayouKurayami <mkw801021@gmail.com>
2020-05-14 08:26:32 -0700

    Increment version number

--- SetParentKK/SetParentLoader.cs
+++ SetParentKK/SetParentLoader.cs
@@ -12,7 +12,7 @@ namespace SetParentKK
 	public class SetParentLoader : BaseUnityPlugin
 	{
 		public const string GUID = "MK.KK_SetParentVR";
-		public const string Version = "2.0.4";
+		public const string Version = "2.1.0";
 		public const string PluginName = "SetParentVR";
 		public const string AssembName = "KK_SetParentVR";
 		internal static SetParent setParentObj;
